6dc3f19868267e12b078a729cf2a2d74a7c69da2
kdgyun <73222685+kdgyun@users.noreply.github.com>
2022-01-26 10:24:24 +0900

    add bench test

---
+++ glob_bench_test/sort_bench_test.go
@@ -0,0 +1,338 @@
+package glob_bench_test
+
+import (
+	"math"
+	"math/big"
+	"math/rand"
+
+	crand "crypto/rand"
+	"testing"
+
+	"github.com/kdgyun/GoSortingAlgorithms/sorts"
+)
+
+func sliceBuilder(len int) []int {
+	seed, _ := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
+	rand.Seed(seed.Int64())
+
+	var a []int
+
+	for i := 0; i < len; i++ {
+		v := rand.Int()
+		a = append(a, v)
+	}
+
+	return a
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.BubbleSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkCocktailSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.CocktailSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkInsertionSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.InsertionSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkSelectionSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.SelectionSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkShellSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ShellSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkBottomUpMergeSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.BottomUpMergeSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkTopDownMergeSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.TopDownMergeSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelMergeSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelMergeSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkHeapSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.HeapSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkQuickSortLP(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.QuickSortLP(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.QuickSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkQuickSortRP(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.QuickSortRP(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelQuickSortLP(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelQuickSortLP(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelQuickSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelQuickSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelQuickSortRP(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelQuickSortRP(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkDualPivotQuickSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.DualPivotQuickSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelDualPivotQuickSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelDualPivotQuickSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkBinarySort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.BinarySort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkTimSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.TimSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkBitonicSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.BitonicSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelBitonicSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelBitonicSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkIntroSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.IntroSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkParallelIntroSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.ParallelIntroSort(test)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkCycleSort(b *testing.B) {
+	b.StopTimer()
+	unsorted := sliceBuilder(100000)
+	test := make([]int, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(test, unsorted)
+		b.StartTimer()
+		sorts.CycleSort(test)
+		b.StopTimer()
+	}
+}
---
+++ glob_bench_test/sort_test.go
@@ -0,0 +1,209 @@
+package glob_bench_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kdgyun/GoSortingAlgorithms/simplytest"
+	"github.com/kdgyun/GoSortingAlgorithms/sorts"
+)
+
+func TestSort(t *testing.T) {
+	size := 1
+	for i := 0; i < 6; i++ {
+		origin, verify := simplytest.RANDOM_INT(size)
+
+		test := make([]int, len(origin))
+		copy(test, origin)
+		t.Run("bubble sort", func(t *testing.T) { sorts.BubbleSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [bubble sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("cocktail sort", func(t *testing.T) { sorts.CocktailSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [cocktail sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("insertion sort", func(t *testing.T) { sorts.InsertionSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [insertion sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("selection sort", func(t *testing.T) { sorts.SelectionSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [selection sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("shell sort", func(t *testing.T) { sorts.ShellSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [shell sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("bottom-up merge sort", func(t *testing.T) { sorts.BottomUpMergeSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [bottom-up merge]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("top-down merge sort", func(t *testing.T) { sorts.TopDownMergeSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [top-down merge]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel merge sort", func(t *testing.T) { sorts.ParallelMergeSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [parallel merge]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("heap sort", func(t *testing.T) { sorts.HeapSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [heap sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("left-pivot quick sort", func(t *testing.T) { sorts.QuickSortLP(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [left-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("middle-pivot quick sort", func(t *testing.T) { sorts.QuickSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [middle-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("right-pivot quick sort", func(t *testing.T) { sorts.QuickSortRP(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [right-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel left-pivot quick sort", func(t *testing.T) { sorts.ParallelQuickSortLP(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [parallel left-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel middle-pivot quick sort", func(t *testing.T) { sorts.ParallelQuickSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [parallel middle-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel right-pivot quick sort", func(t *testing.T) { sorts.ParallelQuickSortRP(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [parallel right-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("dual-pivot quick sort", func(t *testing.T) { sorts.DualPivotQuickSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [dual-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel dual-pivot quick sort", func(t *testing.T) { sorts.ParallelDualPivotQuickSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [parallel dual-pivot quick sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("binary insertion sort", func(t *testing.T) { sorts.BinarySort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [binary insertion sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("tim sort", func(t *testing.T) { sorts.TimSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [tim sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("bitonic sort", func(t *testing.T) { sorts.BitonicSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [bitonic sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel bitonic sort", func(t *testing.T) { sorts.ParallelBitonicSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result [parallel bitonic sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("intro sort", func(t *testing.T) { sorts.IntroSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result intro sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("parallel intro sort", func(t *testing.T) { sorts.ParallelIntroSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result parallel intro sort]")
+		}
+
+		test = make([]int, len(origin))
+		copy(test, origin)
+		t.Run("cycle sort", func(t *testing.T) { sorts.CycleSort(test) })
+
+		if !reflect.DeepEqual(test, verify) {
+			t.Error("Wrong result cycle sort]")
+		}
+		size *= 10
+	}
+}
